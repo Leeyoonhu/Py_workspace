c632169408ec07a6412b6ca6c83996b73264ac7a
Leeyoonhu <316902@naver.com>
2022-08-29 07:42:26 +0900

    first commit

--- solo_practice/IOEx01.py
+++
@@ -1,6 +0,0 @@
-with open("c:/temp/dorian.txt", "r", encoding="utf-8") as openFile:
-    while True:
-        data = openFile.read()
-        print(data, end=" ")
-        if data == "":
-            break
--- solo_practice/practiceEx01.py
+++
@@ -1,8 +0,0 @@
-# 입력한 수의 각 자릿수 2배만큼 * 출력 프로그램 작성
-
-str = input("0이상 수를 입력하세요 >> ")
-
-for i in range(len(str)):
-    for j in range((2 * int(str[i]))):
-        print("*", end="")
-    print()
